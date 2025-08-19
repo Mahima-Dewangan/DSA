a1e8f623ce19ccaecc8df8c536cefbe3989fd4ab
Mahima-Dewangan <mahimadewangan11@gmail.com>
2025-08-19 14:18:00 +0530

    maximum consecutive ones done

---
+++ Arrays/Easy/mximum_consecutive_ones.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+int max_ones(vector<int>arr)
+{
+    int n = arr.size();
+    int count = 0;
+    int maxi=0;
+    for(int i=0;i<n;i++)
+    {
+        if(arr[i]==1)
+        {
+            count++;
+            maxi = max(maxi , count);
+        }
+        else
+        {
+            count=0;
+        }
+    }
+
+    return  maxi;
+
+}
+
+int main()
+{
+    vector<int>arr = {1,1,0,1,1,1,1};
+    cout<<"\nmaximum consecutive ones in a given array is "<<max_ones(arr);
+
+    return 0;
+
+}
